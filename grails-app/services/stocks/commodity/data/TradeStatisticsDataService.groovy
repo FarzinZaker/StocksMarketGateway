5fc8fd5509d53db6a8bfb61930d1cf95607c6ee0
farzin <farzin.zaker@gmail.com>
2014-10-02 11:03:49 +0330

    bug fix

--- grails-app/services/stocks/commodity/data/TradeStatisticsDataService.groovy
+++ grails-app/services/stocks/commodity/data/TradeStatisticsDataService.groovy
@@ -22,7 +22,7 @@ class TradeStatisticsDataService {
                     method : 'importData',
                     trigger: [
                             type      : 'Cron',
-                            parameters: [cronExpression: '0 0 15 2 OCT ? 2014']
+                            parameters: [cronExpression: '0 0 12 2 OCT ? 2014']
                     ]
             ]
     ]
