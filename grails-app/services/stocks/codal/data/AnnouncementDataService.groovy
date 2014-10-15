46150d49e484365f9166d46cf265db15a5c09510
farzin <farzin.zaker@gmail.com>
2014-10-15 14:58:58 +0330

    codal changed

--- grails-app/services/stocks/codal/data/AnnouncementDataService.groovy
+++ grails-app/services/stocks/codal/data/AnnouncementDataService.groovy
@@ -47,46 +47,52 @@ class AnnouncementDataService {
 
     private void parseData(String url) {
 
-        def htmlParser = new XmlSlurper(new Parser()).parseText(getList(url))
-        def containerDiv = htmlParser.'**'.find { it.@id == 'divLetterFormList' }
-        def rows = containerDiv.'**'.findAll { it.name() == 'tr' }
-        rows.remove(0)
-        rows.each { row ->
-            def announcementEvent = new AnnouncementEvent()
-            def cells = row.children()
-            announcementEvent.symbolPersianCode = FarsiNormalizationFilter.apply(cells[0].text() as String)
-            announcementEvent.symbol = Symbol.findByPersianCode(FarsiNormalizationFilter.apply(announcementEvent.symbolPersianCode as String))
-            announcementEvent.companyName = FarsiNormalizationFilter.apply(cells[1].text() as String)
-            announcementEvent.company = Company.findByName(announcementEvent.companyName)
-            announcementEvent.title = FarsiNormalizationFilter.apply(cells[2].text() as String)
-            announcementEvent.sendDate = parseDateTime(cells[3].text())
-            announcementEvent.publishDate = parseDateTime(cells[4].text())
-            def linkUrl = cells[5].@onclick[0].text().replace('window.open(\'', '').replace('\');', '')
-            if (linkUrl && linkUrl != '')
-                announcementEvent.detailsUrl = "http://codal.ir/" + linkUrl
-            linkUrl = cells[6].a.@href
-            if (linkUrl && linkUrl != '')
-                announcementEvent.pdfUrl = "http://codal.ir/" + linkUrl
-            linkUrl = cells[7].a.@href
-            if (linkUrl && linkUrl != '')
-                announcementEvent.excelUrl = "http://codal.ir/" + linkUrl
-            linkUrl = cells[8].a.@href
-            if (linkUrl && linkUrl != '')
-                announcementEvent.xmlUrl = "http://codal.ir/" + linkUrl
-            linkUrl = cells[9].a.@href
-            if (linkUrl && linkUrl != '')
-                announcementEvent.xbrlUrl = linkUrl
-            linkUrl = cells[10].@onclick[0].text().replace('window.open(\'', '').replace('\');', '')
-            if (linkUrl && linkUrl != '')
-                announcementEvent.attachmentsUrl = "http://codal.ir/" + linkUrl
+        try {
+            def htmlParser = new XmlSlurper(new Parser()).parseText(getList(url))
+            def containerDiv = htmlParser.'**'.find { it.@id == 'divLetterFormList' }
+            def rows = containerDiv.'**'.findAll { it.name() == 'tr' }
+            rows.remove(0)
+            rows.each { row ->
+                def announcementEvent = new AnnouncementEvent()
+                def cells = row.children()
+                announcementEvent.symbolPersianCode = FarsiNormalizationFilter.apply(cells[0].text() as String)
+                announcementEvent.symbol = Symbol.findByPersianCode(FarsiNormalizationFilter.apply(announcementEvent.symbolPersianCode as String))
+                announcementEvent.companyName = FarsiNormalizationFilter.apply(cells[1].text() as String)
+                announcementEvent.company = Company.findByName(announcementEvent.companyName)
+                announcementEvent.title = FarsiNormalizationFilter.apply(cells[2].text() as String)
+                announcementEvent.sendDate = parseDateTime(cells[3].text())
+                announcementEvent.publishDate = parseDateTime(cells[4].text())
+                def linkUrl = cells[5].@onclick[0].text().replace('window.open(\'', '').replace('\');', '')
+                if (linkUrl && linkUrl != '')
+                    announcementEvent.detailsUrl = "http://codal.ir/" + linkUrl
+                linkUrl = cells[6].a.@href
+                if (linkUrl && linkUrl != '')
+                    announcementEvent.pdfUrl = "http://codal.ir/" + linkUrl
+                linkUrl = cells[7].a.@href
+                if (linkUrl && linkUrl != '')
+                    announcementEvent.excelUrl = "http://codal.ir/" + linkUrl
+                linkUrl = cells[8].a.@href
+                if (linkUrl && linkUrl != '')
+                    announcementEvent.xmlUrl = "http://codal.ir/" + linkUrl
+                linkUrl = cells[9].a.@href
+                if (linkUrl && linkUrl != '')
+                    announcementEvent.xbrlUrl = linkUrl
+                linkUrl = cells[10].@onclick[0].text().replace('window.open(\'', '').replace('\');', '')
+                if (linkUrl && linkUrl != '')
+                    announcementEvent.attachmentsUrl = "http://codal.ir/" + linkUrl
 
-            announcementEvent.data = find(announcementEvent)
-            codalEventGateway.send(announcementEvent)
+                announcementEvent.data = find(announcementEvent)
+                codalEventGateway.send(announcementEvent)
+            }
+            logState([status:'successful'])
+        }
+        catch (ignored) {
+            logState([status:'failed'])
         }
     }
 
-    private static String getList(String url){
-        String userAgent = RandomUserAgent.randomUserAgent ;
+    private static String getList(String url) {
+        String userAgent = RandomUserAgent.randomUserAgent;
 
         HttpClient client = new HttpClient();
         HttpMethod method = new GetMethod(url);
