25840fb63a1effdb1364cfc8b3dfee04cf5fe819
roohi <srmo642002@gmail.com>
2014-10-18 18:52:44 +0330

    codal changed

--- grails-app/domain/stocks/Chart.groovy
+++ grails-app/domain/stocks/Chart.groovy
@@ -4,6 +4,9 @@ class Chart extends Image {
 
     String options
 
+    static mapping={
+        options sqlType: 'ntext'
+    }
     static constraints = {
         options(maxSize: 20000000)
     }
