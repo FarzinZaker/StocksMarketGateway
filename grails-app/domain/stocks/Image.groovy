38bc2aa4b1880efb1b50be9180229912800c7322
roohi <srmo642002@gmail.com>
2014-10-18 18:50:44 +0330

    codal changed

--- grails-app/domain/stocks/Image.groovy
+++ grails-app/domain/stocks/Image.groovy
@@ -5,6 +5,9 @@ class Image {
     String name
     byte[] content
 
+    static mapping={
+        content sqlType: 'ntext'
+    }
     static constraints = {
         content(maxSize: 2000000000)
     }
