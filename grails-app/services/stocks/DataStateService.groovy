4a448037ffc6fb69c9f3c19778a514c86d22117a
roohi <srmo642002@gmail.com>
2014-10-19 13:26:04 +0330

    codal changed

--- grails-app/services/stocks/DataStateService.groovy
+++ grails-app/services/stocks/DataStateService.groovy
@@ -1,6 +1,7 @@
 package stocks
 
 import grails.converters.JSON
+import groovy.transform.Synchronized
 import org.codehaus.groovy.grails.commons.GrailsClass
 
 class DataStateService {
@@ -17,6 +18,7 @@ class DataStateService {
         }
     }
 
+    @Synchronized
     def initializeStateLogging() {
         serviceNames.each { currentService ->
             def service = grailsApplication.mainContext[currentService.propertyName]
@@ -24,17 +26,17 @@ class DataStateService {
             service.metaClass.logState = { def data ->
 
                 def serviceName = currentService.fullName
-//                DataServiceState.withTransaction {
-//                    DataServiceState.findAllByServiceNameAndIsLastState(serviceName, true).each {
-//                        it.isLastState = false
-//                        it.save()
-//                    }
-//
-//                    DataServiceState state = new DataServiceState()
-//                    state.serviceName = serviceName
-//                    state.data = data as JSON
-//                    state.save()
-//                }
+                DataServiceState.withTransaction {
+                    DataServiceState.findAllByServiceNameAndIsLastState(serviceName, true).each {
+                        it.isLastState = false
+                        it.save()
+                    }
+
+                    DataServiceState state = new DataServiceState()
+                    state.serviceName = serviceName
+                    state.data = data as JSON
+                    state.save()
+                }
             }
 
             service.metaClass.getLastState = {
