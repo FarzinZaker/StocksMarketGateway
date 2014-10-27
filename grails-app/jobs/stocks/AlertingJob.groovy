256291c9ebcdfad529eb5d5c320968a510cae885
farzin <farzin.zaker@gmail.com>
2014-10-27 11:50:53 +0330

    bug fix

--- grails-app/jobs/stocks/AlertingJob.groovy
+++ grails-app/jobs/stocks/AlertingJob.groovy
@@ -1,18 +1,23 @@
 package stocks
 
+import org.springframework.orm.hibernate3.SessionFactoryUtils
 import stocks.alerting.QueryInstance
 
 class AlertingJob {
 
     def queryService
+    def sessionFactory
 
     static startDelay = 60000
     static timeout = 5000l
     static concurrent = false
 
     def execute() {
-        QueryInstance.findAllByNextExecutionTimeLessThanEqualsAndEnabled(new Date(), true).each{queryInstance ->
+        def session = SessionFactoryUtils.getSession(sessionFactory, true)
+        QueryInstance.findAllByNextExecutionTimeLessThanEqualsAndEnabled(new Date(), true).each { queryInstance ->
             queryService.applyScheduledQuery(queryInstance)
         }
+        session.flush()
+        session.clear()
     }
 }
--- grails-app/jobs/stocks/MessageJob.groovy
+++ grails-app/jobs/stocks/MessageJob.groovy
@@ -1,18 +1,23 @@
 package stocks
 
+import org.springframework.orm.hibernate3.SessionFactoryUtils
 import stocks.alerting.QueuedMessage
 
 class MessageJob {
 
     def smsService
+    def sessionFactory
 
     static startDelay = 60000
     static timeout = 5000l
     static concurrent = false
 
     def execute() {
+        def session = SessionFactoryUtils.getSession(sessionFactory, true)
         def message = QueuedMessage.listOrderByLastUpdated([order: 'asc', max: 1])?.find()
-        if(message)
+        if (message)
             smsService.sendMessage(message)
+        session.flush()
+        session.clear()
     }
 }
