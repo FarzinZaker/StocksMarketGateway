2969eca979473f73cfc4d1fdc9f40097c0fbe240
farzin <f_zaker@yahoo.com>
2015-03-19 12:05:13 +0330

    oracle

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -142,7 +142,7 @@ environments {
 
             //Oracle
             dbCreate = "update"
-            url = "jdbc:oracle:thin:@192.168.0.174:1521:CHAHARTABLO"
+            url = "jdbc:oracle:thin:@192.168.64.3:1521:CHAHARTABLO"
             username = "stocks"
             password = "stocks"
             hibernate.default_schema = "stocks"
