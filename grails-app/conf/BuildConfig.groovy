be92a7fa458a3274132226396e410391f3cb3097
farzin <f_zaker@yahoo.com>
2015-03-19 12:24:35 +0330

    oracle

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -27,7 +27,7 @@ grails.project.dependency.resolution = {
         // uncomment these to enable remote dependency resolution from public Maven repositories
         //mavenCentral()
         //mavenLocal()
-
+        mavenRepo "http://repo.grails.org/grails/repo/"
         mavenRepo "http://repository.codehaus.org/"
         mavenRepo "http://snapshots.repository.codehaus.org"
         mavenRepo "http://download.java.net/maven/2/"
