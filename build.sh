3719e83a8da2f3ca6265955e393c98e458fef21c
farzin <f_zaker@yahoo.com>
2015-03-21 23:26:43 +0330

    weblogic problems

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@ export JAVA_HOME=/usr/java/default/
 export GRAILS_HOME=/home/src/grails-2.2.3/
 export JAVA_OPTS="-Xmx2024m -XX:MaxPermSize=512m"
 /home/src/grails-2.2.3/bin/grails war
+cp -uf target/Stocks-0.1.war ../deploy/stocks.war
 
