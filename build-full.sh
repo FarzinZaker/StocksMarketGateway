24492a46a1dc67890218650e0cd672989301046e
farzin <f_zaker@yahoo.com>
2015-04-01 01:41:29 +0430

    build options

--- build-full.sh
+++ build-full.sh
@@ -3,5 +3,5 @@ export GRAILS_HOME=/home/src/grails-2.2.3/
 export JAVA_OPTS="-Xmx2024m -XX:MaxPermSize=512m"
 /home/src/grails-2.2.3/bin/grails clean
 /home/src/grails-2.2.3/bin/grails war
-cp -uf target/Stocks-0.1.war ../deploy/stocks.war
+cp -uf target/Stocks-0.1.war ../../deploy/stocks.war
 
