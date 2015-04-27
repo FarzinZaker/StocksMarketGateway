c1ec7c0078e36cbeac9f269f9281affe4ffa418c
farzin <f_zaker@yahoo.com>
2015-04-27 19:23:55 +0430

    adjustment

--- grails-app/jobs/stocks/PriceAdjustmentJob.groovy
+++ grails-app/jobs/stocks/PriceAdjustmentJob.groovy
@@ -4,6 +4,8 @@ import stocks.tse.SymbolPriceAdjustment
 
 
 class PriceAdjustmentJob {
+
+    def lowLevelDataService
     def priceAdjustmentService
 
     static startDelay = 60000
@@ -12,24 +14,29 @@ class PriceAdjustmentJob {
 
     def execute() {
 
-        def undo = true
-
-        if (undo) {
-            def priceAdjustment = SymbolPriceAdjustment.createCriteria().list {
-                eq('applied', true)
-                order('date', ORDER_ASCENDING)
-                maxResults(1)
-            }?.find()
-            if (priceAdjustment)
-                priceAdjustmentService.undo(priceAdjustment)
-        } else {
-            def priceAdjustment = SymbolPriceAdjustment.createCriteria().list {
-                eq('applied', false)
-                order('date', ORDER_DESCENDING)
-                maxResults(1)
-            }?.find()
-            if (priceAdjustment)
-                priceAdjustmentService.apply(priceAdjustment)
+//        def undo = true
+//
+//        if (undo) {
+//            def priceAdjustment = SymbolPriceAdjustment.createCriteria().list {
+//                eq('applied', true)
+//                order('date', ORDER_ASCENDING)
+//                maxResults(1)
+//            }?.find()
+//            if (priceAdjustment)
+//                priceAdjustmentService.undo(priceAdjustment)
+//        } else {
+//            def priceAdjustment = SymbolPriceAdjustment.createCriteria().list {
+//                eq('applied', false)
+//                order('date', ORDER_DESCENDING)
+//                maxResults(1)
+//            }?.find()
+//            if (priceAdjustment)
+//                priceAdjustmentService.apply(priceAdjustment)
+//        }
+        def result = lowLevelDataService.executeFunction('SYM_SEL_ADJUSTMENT', [:])
+        if (result?.size()) {
+            priceAdjustmentService.apply(result[0].symbolId as Long)
         }
+
     }
 }
--- grails-app/services/stocks/tse/PriceAdjustmentService.groovy
+++ grails-app/services/stocks/tse/PriceAdjustmentService.groovy
@@ -4,35 +4,40 @@ class PriceAdjustmentService {
 
     def symbolIndicatorService
 
-    def apply(SymbolPriceAdjustment priceAdjustment) {
+    def apply(Long symbolId) {
 
         def priceList = SymbolDailyTrade.createCriteria().list {
-            eq('symbolInternalCode', priceAdjustment.symbolInternalCode)
-            lte('dailySnapshot', priceAdjustment.date)
+            symbol {
+                idEq(symbolId)
+            }
+            isNotNull('dailySnapshot')
             order('dailySnapshot', ORDER_DESCENDING)
         }
 
         if (priceList && priceList.size()) {
-            def adjustmentRate = priceAdjustment.adjustedPrice / priceAdjustment.oldPrice
 
-            for (def i = 0; i < priceList.size(); i++)
-                adjustDailyTrade(priceList[i], adjustmentRate)
+            for (def i = 1; i < priceList.size(); i++) {
+                def list = priceList.findAll { it.dailySnapshot == priceList[i].dailySnapshot }
+                for (def j = 0; j < list.size(); j++)
+                    adjustDailyTrade(list[j], priceList[i - 1].yesterdayPrice)
+                i += list.size() - 1
+            }
 
-            symbolIndicatorService.recalculateIndicators(priceAdjustment.symbol)
+            //symbolIndicatorService.recalculateIndicators(priceAdjustment.symbol)
         }
-
-        priceAdjustment.applied = true
-        priceAdjustment.save(flush: true)
     }
 
-    def adjustDailyTrade(SymbolDailyTrade dailyTrade, Double adjustmentRate) {
+    def adjustDailyTrade(SymbolDailyTrade dailyTrade, Double realClosingPrice) {
+        def adjustmentRate = realClosingPrice / dailyTrade.closingPrice
+        if (adjustmentRate == 1)
+            return
         dailyTrade.oldClosingPrice = dailyTrade.closingPrice
-        dailyTrade.closingPrice = Math.round(dailyTrade.closingPrice * adjustmentRate)
-        dailyTrade.firstTradePrice = Math.round(dailyTrade.firstTradePrice * adjustmentRate)
-        dailyTrade.lastTradePrice = Math.round(dailyTrade.lastTradePrice * adjustmentRate)
-        dailyTrade.maxPrice = Math.round(dailyTrade.maxPrice* adjustmentRate)
-        dailyTrade.minPrice = Math.round(dailyTrade.minPrice * adjustmentRate)
-        dailyTrade.yesterdayPrice = Math.round(dailyTrade.yesterdayPrice * adjustmentRate)
+        dailyTrade.closingPrice = realClosingPrice
+        dailyTrade.firstTradePrice = dailyTrade.firstTradePrice * adjustmentRate
+        dailyTrade.lastTradePrice = dailyTrade.lastTradePrice * adjustmentRate
+        dailyTrade.maxPrice = dailyTrade.maxPrice * adjustmentRate
+        dailyTrade.minPrice = dailyTrade.minPrice * adjustmentRate
+        dailyTrade.yesterdayPrice = dailyTrade.yesterdayPrice * adjustmentRate
         dailyTrade.save(flush: true)
     }
 
@@ -62,7 +67,7 @@ class PriceAdjustmentService {
         dailyTrade.closingPrice = Math.round(dailyTrade.closingPrice / adjustmentRate)
         dailyTrade.firstTradePrice = Math.round(dailyTrade.firstTradePrice / adjustmentRate)
         dailyTrade.lastTradePrice = Math.round(dailyTrade.lastTradePrice / adjustmentRate)
-        dailyTrade.maxPrice = Math.round(dailyTrade.maxPrice/ adjustmentRate)
+        dailyTrade.maxPrice = Math.round(dailyTrade.maxPrice / adjustmentRate)
         dailyTrade.minPrice = Math.round(dailyTrade.minPrice / adjustmentRate)
         dailyTrade.yesterdayPrice = Math.round(dailyTrade.yesterdayPrice / adjustmentRate)
         dailyTrade.save(flush: true)
