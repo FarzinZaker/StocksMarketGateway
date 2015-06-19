2e6d6774a3fefa7dfe86a204f704abc2c284f51e
farzin <farzin.zaker@gmail.com>
2015-06-19 19:59:25 +0430

    initial daily trades migration

--- grails-app/jobs/stocks/DailyTradesToTimeSeriesJob.groovy
+++ grails-app/jobs/stocks/DailyTradesToTimeSeriesJob.groovy
@@ -13,6 +13,7 @@ class DailyTradesToTimeSeriesJob {
     def adjustedPriceSeriesService
 
     def execute() {
+        return
         def lastState = getLastState()
         println """remaining items: ${
             SymbolDailyTrade.createCriteria().count {
