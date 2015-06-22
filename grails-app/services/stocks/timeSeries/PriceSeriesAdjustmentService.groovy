41919df954c59e62f59411fbe533e1cb9c73cba9
farzin <123456>
2015-06-22 20:08:30 +0430

    time series correction

--- grails-app/services/stocks/timeSeries/PriceSeriesAdjustmentService.groovy
+++ grails-app/services/stocks/timeSeries/PriceSeriesAdjustmentService.groovy
@@ -24,10 +24,10 @@ class PriceSeriesAdjustmentService {
         if (dailyTrades && dailyTrades.size()) {
 
             def finalList = []
-            for (def i = 1; i < dailyTrades.size(); i++) {
-                def adjustmentRate = dailyTrades[i - 1].yesterdayPrice / dailyTrades[i].closingPrice
+            for (def i = 0; i < dailyTrades.size() - 1; i++) {
+                def adjustmentRate = dailyTrades[i + 1].yesterdayPrice / dailyTrades[i].closingPrice
                 if (adjustmentRate != 1) {
-                    dailyTrades[i].closingPrice = dailyTrades[i - 1].yesterdayPrice
+                    dailyTrades[i].closingPrice = dailyTrades[i + 1].yesterdayPrice
                     dailyTrades[i].firstTradePrice = Math.round((dailyTrades[i].firstTradePrice * adjustmentRate) as Double)
                     dailyTrades[i].lastTradePrice = Math.round((dailyTrades[i].lastTradePrice * adjustmentRate) as Double)
                     dailyTrades[i].maxPrice = Math.round((dailyTrades[i].maxPrice * adjustmentRate) as Double)
