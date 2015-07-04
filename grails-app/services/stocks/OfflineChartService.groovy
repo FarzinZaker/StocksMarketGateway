e141beaae69eced2114f740139c72cab0a440203
farzin <farzin.zaker@gmail.com>
2015-07-05 00:21:00 +0430

    heatmap

--- grails-app/services/stocks/OfflineChartService.groovy
+++ grails-app/services/stocks/OfflineChartService.groovy
@@ -2,12 +2,6 @@ package stocks
 
 import org.codehaus.groovy.grails.web.context.ServletContextHolder
 
-import java.io.*;
-
-import org.apache.batik.transcoder.image.JPEGTranscoder;
-import org.apache.batik.transcoder.TranscoderInput;
-import org.apache.batik.transcoder.TranscoderOutput;
-
 class OfflineChartService{
 
     def heatMap(){
@@ -15,28 +9,6 @@ class OfflineChartService{
         def proc = command.execute()
         proc.waitFor()
 
-        // Create a JPEG transcoder
-        JPEGTranscoder t = new JPEGTranscoder();
-
-        // Set the transcoding hints.
-        t.addTranscodingHint(JPEGTranscoder.KEY_QUALITY,
-                new Float(.8));
-
-        // Create the transcoder input.
-        String svgURI = new File(args[0]).toURL().toString();
-        TranscoderInput input = new TranscoderInput(svgURI);
-
-        // Create the transcoder output.
-        OutputStream ostream = new FileOutputStream("out.jpg");
-        TranscoderOutput output = new TranscoderOutput(ostream);
-
-        // Save the image.
-        t.transcode(input, output);
-
-        // Flush and close the stream.
-        ostream.flush();
-        ostream.close();
-
-        println "heatMap created"
+        println "heatMap created in ${ServletContextHolder.servletContext.getRealPath('/')}heat-map/heatMap.svg"
     }
 }
