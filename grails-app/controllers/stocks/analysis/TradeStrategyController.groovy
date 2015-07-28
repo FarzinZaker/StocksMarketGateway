79054dd992ed7915ec9371b257649071ae252da4
farzin <farzin>
2015-07-28 15:00:02 +0430

    backtest

--- grails-app/controllers/stocks/analysis/TradeStrategyController.groovy
+++ grails-app/controllers/stocks/analysis/TradeStrategyController.groovy
@@ -57,7 +57,7 @@ class TradeStrategyController {
     def queryItem() {
         def value = params.findAll { it.key.toString().contains('value') }.collect {
             it.value == 'on' ? it.key.toString().replace('value_', '') : it.value
-        }
+        }?.sort{it[0]}
         render template: 'queryItem', model: [text: filterService.getQueryText(params.filter?.toString(), params.operator, value), filter: params.filter, parameter: params.parameter, operator: params.operator, value: value as JSON]
     }
 
