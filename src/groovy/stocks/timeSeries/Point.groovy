322359f86e66d61c2f18ee5e0bac44bd8cc9f07a
farzin <farzin>
2015-08-15 19:25:09 +0430

    new method of time series

--- src/groovy/stocks/timeSeries/Point.groovy
+++ src/groovy/stocks/timeSeries/Point.groovy
@@ -40,7 +40,7 @@ public class Point {
 
         def utc = date.clearTime()
         use(TimeCategory){
-             utc = utc + 1.day
+             utc = utc + 1.days
         }
         timeLong = utc.time
         this
