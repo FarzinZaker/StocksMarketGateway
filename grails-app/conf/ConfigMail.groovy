23b2195c07c58083a775acc43f0ca6a9c84d1265
farzin <farzin>
2015-09-27 19:27:29 +0330

    mail server migrated

--- grails-app/conf/ConfigMail.groovy
+++ grails-app/conf/ConfigMail.groovy
@@ -1,6 +1,4 @@
-
-
-grails.mail.default.from="4tablo <no-reply@4tablo.ir>"
+grails.mail.default.from = "4tablo <no-reply@4tablo.ir>"
 grails {
     mail {
         ssl = "off"
@@ -11,7 +9,11 @@ grails {
         username = "no-reply@4tablo.ir"
         password = 'agah!#($'
         props = [
-                "mail.debug":"true",
+                "mail.debug"             : "true",
+                "mail.transport.protocol": "smtp",
+                "mail.smtp.host"         : "mail.4tablo.ir",
+                "mail.smtp.port"         : "25",
+                "mail.smtp.auth"         : "true"
         ]
 
     }
