db37869d614e6ada7ea6c600a3d7cbf5359b877f
farzin <farzin>
2015-09-30 16:05:05 +0330

    commodity market problem

--- grails-app/services/stocks/commodity/data/CommodityMarketActivityDataService.groovy
+++ grails-app/services/stocks/commodity/data/CommodityMarketActivityDataService.groovy
@@ -32,14 +32,14 @@ class CommodityMarketActivityDataService {
 
         def client = new DefaultHttpClient()
         def get = new HttpHead("http://www.ime.co.ir")
-        get.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0")
-        get.setHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-        get.setHeader("Accept-Encoding", "gzip, deflate")
-        get.setHeader("Accept-Language", "en-US,en;q=0.5")
-        get.setHeader("Cookie", "ASP.NET_SessionId=fbw4uacvssm2afrgwuiarmjh")
-        get.setHeader("Host", "www.ime.co.ir")
-        get.setHeader("Referer", "http://www.ime.co.ir/auction-total-report.html")
-        get.setHeader("Content-Type", "application/json; charset=utf-8")
+//        get.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0")
+//        get.setHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+//        get.setHeader("Accept-Encoding", "gzip, deflate")
+//        get.setHeader("Accept-Language", "en-US,en;q=0.5")
+//        get.setHeader("Cookie", "ASP.NET_SessionId=fbw4uacvssm2afrgwuiarmjh")
+//        get.setHeader("Host", "www.ime.co.ir")
+//        get.setHeader("Referer", "http://www.ime.co.ir/auction-total-report.html")
+//        get.setHeader("Content-Type", "application/json; charset=utf-8")
         def response = client.execute(get)
         def sessionId = response.headergroup.headers.find {
             it.name?.toLowerCase() == 'set-cookie'
